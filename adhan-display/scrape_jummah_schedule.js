59dc5d8c5796f145a0c07865e05600809796fcb6
SyButter <sbadar2k@gmail.com>
2025-05-19 20:20:51 -0400

    Updated the chromium path for rasppi

--- adhan-display/scrape_jummah_schedule.js
+++ adhan-display/scrape_jummah_schedule.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 
 ;(async () => {
   const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/chromium-browser',
     args: ['--no-sandbox','--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
